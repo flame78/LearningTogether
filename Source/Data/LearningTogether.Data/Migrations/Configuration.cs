542ddb83ee817c34398b9789dac055632c86c4b7
flame78 <flame78@gmail.com>
2016-02-24 14:54:10 +0200

    bug

--- Source/Data/LearningTogether.Data/Migrations/Configuration.cs
+++ Source/Data/LearningTogether.Data/Migrations/Configuration.cs
@@ -52,7 +52,7 @@
                         Category = categories[7],
                         Description =
                                 i
-                                + " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                                + " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
                         Title = "Lorem Ipsum" + i,
                         ISBN = "978-3-16-148410-" + i,
                     });
@@ -70,7 +70,7 @@
                             {
                                 Author = user,
                                 Title = "None",
-                                Description = "For now this nothing!",
+                                Description = "For now this nothing    !",
                                 Content = "<h1>very important</h1>",
                                 Category = categories[7]
                             },
@@ -93,7 +93,7 @@
                         Category = categories[7],
                         Description =
                                 i
-                                + " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                                + " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
                         Title = "Lorem Ipsum" + i,
                     });
             }
@@ -118,7 +118,7 @@ and are likewise accessible free on YouTube.com. ",
                                    new ExternalItem()
                                        {
                                            Author = user,
-                                           Description = @"Learn English Online",
+                                           Description = @"Learn English Online   ",
                                            Link = "http://www.english-online.org.uk/",
                                            Type = ExternalItemType.Site,
                                            Category = categories[5]
